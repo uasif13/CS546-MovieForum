1ea8689f97b61b00a9939748531998471eaca495
jordang7 <77jgreenberg77@gmail.com>
2020-12-11 00:02:44 -0500

    added movie detail page and recommendations list

--- public/js/trending.js
+++ public/js/trending.js
@@ -7,10 +7,12 @@ $(document).ready(function () {
     },
     url: "http://api.themoviedb.org/3/movie/popular",
   })
-    .then(function (shows) {
-      $.each(shows.results, function (i, data) {
+    .then(function (movies) {
+      $.each(movies.results, function (i, data) {
         counter++;
-        $("#movieList").append("<li class='link'>" + data.title + "</li>");
+        $("#movieList").append(
+          "<li class='link'><a href=" + data.id + ">" + data.title + "</a></li>"
+        );
       });
     })
     .then(function () {
@@ -18,3 +20,97 @@ $(document).ready(function () {
     });
 });
 // apikey 2c5709ff90e8aeb0f12febf13b682fa8
+
+(function ($) {
+  $("#movieList").on("click", "li.link a", function (event) {
+    event.preventDefault();
+    $("#movieList").css({ display: "none" });
+    $("#movie").empty();
+    let currentId = $(this).attr("href");
+    $.ajax({
+      method: "GET",
+      data: {
+        api_key: "2c5709ff90e8aeb0f12febf13b682fa8",
+      },
+      url: "http://api.themoviedb.org/3/movie/" + currentId,
+    })
+      .then(function (data) {
+        if (!data.title) {
+          data.title = "N/A";
+        }
+        if (!data.genres) {
+          data.genres = "N/A";
+        }
+        if (!data.overview) {
+          data.overview = "N/A";
+        }
+        if (!data.budget) {
+          data.budget = "N/A";
+        }
+        if (!data.vote_average) {
+          data.vote_average = "N/A";
+        }
+        $("#movie").append("<h1>" + data.title + "</h1>");
+
+        if (!data.image) {
+          $("#show").append(
+            "<img src=" + "../public/no_image.jpeg" + "></img>"
+          );
+        } else {
+          $("#show").append("<img src=" + data.image.medium + "></img>");
+        }
+        $("#movie").append(
+          "<dl class='defList' ><dt>Title </dt><dd>" +
+            data.title +
+            "</dd>" +
+            "<dt>Genres</dt><dd>" +
+            makeUnorderedList(data.genres).innerHTML +
+            "</dd><dt>Overview</dt><dd>" +
+            data.overview +
+            "</dd>" +
+            "</dd><dt>Budget</dt><dd>" +
+            data.budget +
+            "</dd>" +
+            "</dd><dt>vote_average</dt><dd>" +
+            data.vote_average +
+            "</dd></dl>"
+        );
+        $.ajax({
+          method: "GET",
+          data: {
+            api_key: "2c5709ff90e8aeb0f12febf13b682fa8",
+          },
+          url:
+            "http://api.themoviedb.org/3/movie/" +
+            currentId +
+            "/recommendations",
+        }).then(function (movies) {
+          console.log(movies);
+          $.each(movies.results, function (i, data) {
+            counter++;
+            $("#recommendations").append(
+              "<li class='link'><a href=" +
+                data.id +
+                ">" +
+                data.title +
+                "</a></li"
+            );
+          });
+        });
+      })
+      .then(function () {
+        $("#movie").css({ display: "block" });
+        $("#homeLink").css({ display: "block" });
+        $("#recommendations").css({ display: "block" });
+      });
+  });
+  function makeUnorderedList(array) {
+    var list = document.createElement("ul");
+    for (var i = 0; i < array.length; i++) {
+      var item = document.createElement("li");
+      item.appendChild(document.createTextNode(array[i]));
+      list.appendChild(item);
+    }
+    return list;
+  }
+})(window.jQuery);

af9c2db9a57e3281f8a05ca054c20fd3e5eb7d65
uasif13 <uasif13@gmail.com>
2020-12-12 13:22:11 -0500

    added html form for search

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const mainRoutes = require('./main')
+const searchRoutes = require('./search')
 // Insert separate routes for the different functions
 
 const constructor = (app) => {
     // Add routes with app.use
     app.use("/", mainRoutes)
+    app.use("/search", searchRoutes)
     app.use("*", (req,res) => {
         res.status(404).json({error: "Not found"});
     });

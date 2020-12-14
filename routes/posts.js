10bf46f800ace7d93644f3cff3bd8fdd88b89d8a
vineethvs23 <vinetthvs2301@gmail.com>
2020-12-14 12:29:50 -0500

    new get route (by id) in posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,30 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const data = require("../data/index")
+const data = require("../data/index");
 const movies = data.movies;
 const postsData = data.posts;
-
+const commentsData = data.comments;
 
 // This should render a page to create a post
 router.get("/movieSelection", async (req, res) => {
-    res.render("partials/createPost", {title: "Create a post"})
-})
-router.post("/", async (req, res) => {
-    
-})
-module.exports = router
+  res.render("partials/createPost", { title: "Create a post" });
+});
+router.post("/", async (req, res) => {});
+
+router.get("/");
+
+router.get("/:id", async (req, res) => {
+  try {
+    let postList = await postsData.getPost(req.params.id);
+    let allComments = commentsData.getAllComments(req.params.id);
+    res
+      .status(200)
+      .render("partials/postPage", { post: postList, comments: allComments });
+  } catch (e) {
+    res
+      .status(404)
+      .redirect("partials/allPosts", { errorMessage: "Post could be found" });
+  }
+});
+
+module.exports = router;

d50bd41d13f569d36d3bb076d755a0356fd45a40
jordang7 <77jgreenberg77@gmail.com>
2020-12-17 12:37:47 -0500

    added recommendation func to the movie detail page

--- public/js/trending.js
+++ public/js/trending.js
@@ -4,23 +4,39 @@ $(document).ready(function () {
     $("#post").css({ display: "none" });
     $("#recommendations").empty();
     let currentId = $(this).attr("href");
+    let title = $("#title");
     $.ajax({
       method: "GET",
       data: {
         api_key: "2c5709ff90e8aeb0f12febf13b682fa8",
+        query: title.text(),
+        language: "en-US",
+        page: 1,
       },
-      url: "http://api.themoviedb.org/3/movie" + currentId + "/recommendations",
-    })
-      .then(function (movies) {
-        $.each(movies.results, function (i, data) {
-          $("#recommendations").append("<li>" + data.title + "</a></li");
-        });
+      url: "http://api.themoviedb.org/3/search/movie",
+    }).then(function (movies) {
+      $.ajax({
+        method: "GET",
+        data: {
+          api_key: "2c5709ff90e8aeb0f12febf13b682fa8",
+        },
+        url:
+          "http://api.themoviedb.org/3/movie/" +
+          movies.results[0].id +
+          "/recommendations",
       })
-      .then(function () {
-        $("#reccomendations").css({ display: "block" });
-        $("#rec").css({ display: "none" });
-        $("#postLink").css({ display: "block" });
-        $("#recTitle").css({ display: "block" });
-      });
+        .then(function (recs) {
+          console.log(recs);
+          $.each(recs.results, function (i, data) {
+            $("#recommendations").append("<li>" + data.title + "</a></li");
+          });
+        })
+        .then(function () {
+          $("#reccomendations").css({ display: "block" });
+          $("#rec").css({ display: "none" });
+          $("#postLink").css({ display: "block" });
+          $("#recTitle").css({ display: "block" });
+        });
+    });
   });
 });

eaf4073c10881261c940525934f17502d59cde8a
uasif13 <uasif13@gmail.com>
2020-12-17 02:32:45 -0500

    asif: finished ajax form submission and client side validation of createMovies

--- routes/movies.js
+++ routes/movies.js
@@ -1,74 +1,84 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 const data = require("../data/index");
 const moviesData = data.movies;
-const axios = require('axios');
+const axios = require("axios");
 const movies = require("../data/movies");
-const API_KEY = '2c5709ff90e8aeb0f12febf13b682fa8' 
+const API_KEY = "2c5709ff90e8aeb0f12febf13b682fa8";
 const spaceRegex = /^\s*$/;
-const xss = require("xss")
+const xss = require("xss");
 
 // GEt the createMovie page
-router.get('/create', async (req, res) => {
-    try {
-        if (!req.session) {
-            throw "There is no session"
-        }
-        if (!req.session.user) {
-            throw "You must be logged in before you can make a search"
-        }
-        res.render('partials/createMovie', {title: "Create a movie"} );
-    } catch(e) {
-        res.status(500).send(e)
+router.get("/create", async (req, res) => {
+  try {
+    if (!req.session) {
+      throw "There is no session";
     }
-})
+    if (!req.session.user) {
+      throw "You must be logged in before you can make a movie";
+    }
+    res.render("partials/createMovie", { title: "Create a movie" });
+  } catch (e) {
+    res.redirect("/");
+  }
+});
 
 // GEt all movies
-router.get('/', async (req, res) => {
-    try {
-        if (!req.session) {
-            throw "There is no session"
-        }
-        if (!req.session.user) {
-            throw "You must be logged in before you can make a search"
-        }
-        let allMovies = await moviesData.getAllMovies();
-        res.json(allMovies);
-    } catch(e) {
-        res.status(500).send(e)
+router.get("/", async (req, res) => {
+  try {
+    if (!req.session) {
+      throw "There is no session";
     }
-})
+    if (!req.session.user) {
+      throw "You must be logged in before you can see a movie";
+    }
+    let allMovies = await moviesData.getAllMovies();
+    res.json(allMovies);
+  } catch (e) {
+    // res.status(500).send(e)
+    res.redirect("/");
+  }
+});
 // Movie is created from user input
-router.post('/', async (req, res) => {
-    try {
-        if (!req.session) {
-            throw "There is no session"
-        }
-        if (!req.session.user) {
-            throw "You must be logged in before you can make a search"
-        }
-        if (!req.body) {
-        throw "No body was sent with POST request"
-        }
-        let data = req.body;
-        let addedMovie = {};
-        if (req.body.title && req.body.description && req.body.genres && req.body.image && req.body.budget) {
-            data.genres.forEach(genre => {
-                xss(genre)
-            });
-            addedMovie = await moviesData.createMovie(
-                xss(data.title), 
-                xss(data.description), 
-                data.genres, 
-                xss(data.budget), 
-                xss(data.image))
-        }
-        res.render('partials/showDetails', {title: "Show Details", show: addedMovie});
-    } catch (e) {
-        res.status(500).send(e);
+router.post("/", async (req, res) => {
+  try {
+    if (!req.session) {
+      throw "There is no session";
     }
-
-})
+    if (!req.session.user) {
+      throw "You must be logged in before you can make a search";
+    }
+    if (!req.body) {
+      throw "No body was sent with POST request";
+    }
+    let data = req.body;
+    let addedMovie = {};
+    if (
+      req.body.title &&
+      req.body.description &&
+      req.body.genres &&
+      req.body.image &&
+      req.body.budget
+    ) {
+      data.genres.forEach((genre) => {
+        xss(genre);
+      });
+      addedMovie = await moviesData.createMovie(
+        xss(data.title),
+        xss(data.description),
+        data.genres,
+        xss(data.budget),
+        xss(data.image)
+      );
+    }
+    res.render("partials/showDetails", {
+      title: "Show Details",
+      show: addedMovie,
+    });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 // Movie is created from TMDB json object
 // This is extra. I will save this if I plan to do this in future. Since it's not on the extra or required feature, its not needed
 // router.post('/:id', async (req, res) => {

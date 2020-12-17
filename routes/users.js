9381c67e1231d5f6fc26cefb9f0a2174dc2696d1
Avirat <abelekar@stevens.edu>
2020-12-17 13:47:08 -0500

    updated the user profile page

--- routes/users.js
+++ routes/users.js
@@ -7,22 +7,21 @@ router.get('/update', (req,res,next) => {
     if(!req.params) {
         return next();
     }
-
-    const users = req.app.locals.users;
-    const _id = ObjectID(req.session.passport.user);
-    
-    users.findOne({_id} , (err,results) => {
-        if (err) {
-            throw err;
-        }
-        res.render('account', { ...results});
-    });
+    //const users = req.app.locals.users;
+    //const _id = ObjectID(req.session.passport.user);
+    const userId = req.session.user._id;
+    try{
+        let user = userMethods.getUserByID(userId);
+        res.render('partials/account', {user:user});
+    }
+     catch(e){
+     }   
 });
 
 router.get('/' , (req, res,next) => {
     const users = req.app.locals.users;
     const username = req.params.username;
-    console.log(req.session.userId);
+    //console.log(req.session.userId);
     const userId = req.session.user._id;
     try{
         let user =  userMethods.getUserByID(userId);
@@ -33,6 +32,7 @@ router.get('/' , (req, res,next) => {
 
     }
 });
+
 
 router.post('/', (req, res, next) => {
     if(!req.params){
